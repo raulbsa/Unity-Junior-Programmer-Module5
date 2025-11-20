aba00226e4ced86db7243c32e099e6be52464878
raulbsa <raul.victor@bsatech.io>
2025-11-20 16:48:12 -0300

    Challenge completo

---
+++ Assets/Challenge 5/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using System;
+using TMPro;
+using UnityEngine;
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 60;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+    public GameManagerX gameManagerX;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        //timerIsRunning = true;
+        gameManagerX.OnDificuldadeSelecionada += IniciarTimer;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                gameManagerX.GameOver();
+            }
+        }
+    }
+
+    private void IniciarTimer()
+    {
+        timerIsRunning = true;
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = "Timer: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
